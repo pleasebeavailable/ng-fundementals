3523bd7130d37061affba3b28d5455620a0c4d87
Ante Sucur <ante_sucur@hotmail.com>
2020-09-08 15:47:39 +0200

    Create event and toastr service

---
+++ src/app/events-list/shared/event.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  constructor() { }
+
+  getEvents() {
+    return null;
+  }
+}
