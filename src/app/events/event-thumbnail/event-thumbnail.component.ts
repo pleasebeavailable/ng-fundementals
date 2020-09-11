491ecf7a671b28e9f82fe2149b27c591b0c313ca
Ante Sucur <ante_sucur@hotmail.com>
2020-09-11 11:33:54 +0200

    Introduce data models ( IEvent & ISession )

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {IEvent} from '../shared';
 
 @Component({
   selector: 'app-event-thumbnail',
@@ -6,7 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./event-thumbnail.component.css']
 })
 export class EventThumbnailComponent implements OnInit {
-  @Input() event: any;
+  @Input() event: IEvent;
 
   constructor() {
   }
