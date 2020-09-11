b63afe95e29039d554e14190b59bcf7de13a9bb2
Ante Sucur <ante_sucur@hotmail.com>
2020-09-11 11:04:46 +0200

    Add barrel export organization and restructure directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {
+  EventsListComponent, EventService, EventDetailsComponent, CreateEventComponent, EventThumbnailComponent, EventRouteActivatorService
+} from './events/index';
+
 import {AppComponent} from './app.component';
-import {EventsListComponent} from './events/events-list.component';
-import {EventThumbnailComponent} from './events/event-thumbnail/event-thumbnail.component';
 import {NavbarComponent} from './nav/navbar/navbar.component';
-import {EventService} from './events/shared/event.service';
 import {ToastrService} from './common/toastr.service';
-import {EventDetailsComponent} from './events/event-details/event-details.component';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './routes';
-import { CreateEventComponent } from './events/event-form/create-event/create-event.component';
 import {Error404Component} from './errors/404.component';
-import {EventRouteActivatorService} from './events/event-details/event-route-activator.service';
 
 @NgModule({
   declarations: [
---
+++ src/app/events/index.ts
@@ -0,0 +1,6 @@
+export * from './events-list-resolver.service';
+export * from './events-list.component';
+export * from './shared/index';
+export * from './event-details/index';
+export * from './create-event/index';
+export * from './event-thumbnail/index';
