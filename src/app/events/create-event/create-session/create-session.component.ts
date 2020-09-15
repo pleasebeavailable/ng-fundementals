538992a081cf8426a25a651cc33a03723a84fe02
Ante Sucur <ante_sucur@hotmail.com>
2020-09-15 14:53:53 +0200

    Add create session functionality on child component (session-list)

--- src/app/events/create-event/create-session/create-session.component.ts
+++ src/app/events/create-event/create-session/create-session.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ISession, restrictedWords} from '../../shared';
 
@@ -8,6 +8,8 @@ import {ISession, restrictedWords} from '../../shared';
   styleUrls: ['./create-session.component.css']
 })
 export class CreateSessionComponent implements OnInit {
+  @Output() saveNewSession = new EventEmitter();
+  @Output() cancelAddSession = new EventEmitter();
   newSessionForm: FormGroup;
   name: FormControl;
   presenter: FormControl;
@@ -48,6 +50,10 @@ export class CreateSessionComponent implements OnInit {
       voters: []
     };
 
-    console.log(session);
+    this.saveNewSession.emit(session);
+  }
+
+  cancel() {
+    this.cancelAddSession.emit();
   }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,16 +5,21 @@ import {CreateEventComponent} from './events/create-event/create-event.component
 import {Error404Component} from './errors/404.component';
 import {EventRouteActivatorService} from './events/event-details/event-route-activator.service';
 import {EventsListResolverService} from './events/events-list-resolver.service';
+import {CreateSessionComponent} from './events/create-event';
 
 export const appRoutes: Routes = [
   {
-    path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']
+    path: 'events/new', component: CreateEventComponent,
+    // canDeactivate: ['canDeactivateCreateEvent']
   },
   {
     path: 'events', component: EventsListComponent, resolve: {events: EventsListResolverService}
   },
   {
     path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivatorService]
+  },
+  {
+    path: 'events/session/new', component: CreateSessionComponent
   },
   {
 
